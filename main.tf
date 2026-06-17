5cd82dd302d87b83d70670a1c605d70b711f35d2
panlingnan <panlingnan@bytedance.com>
2026-06-17 14:51:50 +0800

    var

--- main.tf
+++ main.tf
@@ -6,40 +6,98 @@ terraform {
   }
 }
 
-provider "volcenginecc" {
-  endpoints = {
-    cloudcontrolapi = "open.stable.volcengineapi-test.com"
-  }
-
-  region     = "cn-guilin-boe"
+variable "region" {
+  description = "Volcengine region"
+  type        = string
+  default     = "cn-guilin-boe"
 }
 
-resource "volcenginecc_vpc_vpc" "vpc-demo" {
-  vpc_name    = "vpc-demo"
-  description = "VPC created by Terraform"
-  cidr_block  = "192.168.0.0/16"
+variable "cloudcontrolapi_endpoint" {
+  description = "Cloud Control API endpoint"
+  type        = string
+  default     = "open.stable.volcengineapi-test.com"
+}
 
-  tags = [
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "vpc-demo"
+}
+
+variable "vpc_description" {
+  description = "Description of the VPC"
+  type        = string
+  default     = "VPC created by Terraform"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "subnet-demo"
+}
+
+variable "subnet_description" {
+  description = "Description of the subnet"
+  type        = string
+  default     = "Subnet created by Terraform"
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block of the subnet"
+  type        = string
+  default     = "192.168.1.0/24"
+}
+
+variable "subnet_zone_id" {
+  description = "Availability zone ID of the subnet"
+  type        = string
+  default     = "cn-guilin-a"
+}
+
+variable "tags" {
+  description = "Tags applied to the VPC and subnet"
+  type = list(object({
+    key   = string
+    value = string
+  }))
+  default = [
     {
       key   = "env"
       value = "test"
     }
   ]
+}
+
+provider "volcenginecc" {
+  endpoints = {
+    cloudcontrolapi = var.cloudcontrolapi_endpoint
+  }
+
+  region = var.region
+}
+
+resource "volcenginecc_vpc_vpc" "vpc-demo" {
+  vpc_name    = var.vpc_name
+  description = var.vpc_description
+  cidr_block  = var.vpc_cidr_block
+
+  tags = var.tags
 }
 
 resource "volcenginecc_vpc_subnet" "subnet-demo" {
   vpc_id      = volcenginecc_vpc_vpc.vpc-demo.id
-  zone_id     = "cn-guilin-a"
-  subnet_name = "subnet-demo"
-  description = "Subnet created by Terraform"
-  cidr_block  = "192.168.1.0/24"
+  zone_id     = var.subnet_zone_id
+  subnet_name = var.subnet_name
+  description = var.subnet_description
+  cidr_block  = var.subnet_cidr_block
 
-  tags = [
-    {
-      key   = "env"
-      value = "test"
-    }
-  ]
+  tags = var.tags
 }
 
 output "vpc_id" {
